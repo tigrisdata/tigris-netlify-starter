a3c835c6d12582a51cb594b6b8628c1bf5681987
Ovais Tariq <ot@tigrisdata.com>
2023-01-19 13:59:08 -0800

    Update the starter app with the latest changes in Tigris SDK

--- pages/api/items/search.ts
+++ pages/api/items/search.ts
@@ -1,35 +1,29 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { COLLECTION_NAME, TodoItem } from '../../../models/tigris/tigris_netlify_starter/todoItems'
-import { SearchRequest } from '@tigrisdata/core/dist/search/types'
-import { Collection } from '@tigrisdata/core'
-import tigrisDb from '../../../lib/tigris'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { TodoItem } from '../../../db/models/todoItems';
+import tigrisDb from '../../../lib/tigris';
 
 type Data = {
-  result?: Array<TodoItem>,
-  error?: string
-}
+  result?: Array<TodoItem>;
+  error?: string;
+};
 
-// GET /api/items/search?q=searchQ -- searches for items matching `searchQ`
-export default async function handler (
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const query = req.query['q']
+// GET /api/items/search?q=searchQ -- searches for items matching text `searchQ`
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const query = req.query['q'];
   if (query === undefined) {
-    res.status(400).json({ error: 'No search query found in request' })
-    return
+    res.status(400).json({ error: 'No search query found in request' });
+    return;
   }
   try {
-    const collection: Collection<TodoItem> = tigrisDb.getCollection(COLLECTION_NAME)
-    const searchRequest: SearchRequest<TodoItem> = { q: query as string }
-    const searchResult = await collection.search(searchRequest)
-    const items = new Array<TodoItem>()
-    for (const hit of searchResult.hits) {
-      items.push(hit.document)
+    const itemsCollection = tigrisDb.getCollection<TodoItem>(TodoItem);
+    const searchResult = await itemsCollection.search({ q: query as string });
+    const items = new Array<TodoItem>();
+    for await (const res of searchResult) {
+      res.hits.forEach(hit => items.push(hit.document));
     }
-    res.status(200).json({ result: items })
+    res.status(200).json({ result: items });
   } catch (err) {
-    const error = err as Error
-    res.status(500).json({ error: error.message })
+    const error = err as Error;
+    res.status(500).json({ error: error.message });
   }
 }
